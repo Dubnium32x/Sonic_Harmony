e87267632c162715993e22f1a093c70525b0b55b
Dubnium32x <63617522+Dubnium32x@users.noreply.github.com>
2020-07-15 14:18:23 -0700

    pushing noah's changes

--- Project Presto/Assets/Scripts/CharaterScripts/States/LedgeGrabStateBack.cs
+++ Project Presto/Assets/Scripts/CharaterScripts/States/LedgeGrabStateBack.cs
@@ -6,41 +6,35 @@ public class LedgeGrabStateBack : PlayerState
 {
     public override void Step(CharControlMotor player, float deltaTime)
     {
+        player.UpdateDirection(player.input.horizontal);
+        player.HandleSlopeFactor(deltaTime);
+        player.HandleAcceleration(deltaTime);
+        player.HandleFriction(deltaTime);
+        player.HandleGravity(deltaTime);
+        player.HandleFall();
         if (!player.HandleLedgeCheck().Item2)
         {
             player.state.ChangeState<WalkPlayerState>();
         }
+
         if (player.input.actionDown)
         {
             player.HandleJump();
         }
+
         if (player.HandleLedgeCheck().Item1)
         {
             player.state.ChangeState<LedgeGrabStateFront>();
         }
-        if (player.input.horizontal != 0)
+
+        if (player.input.horizontal != 0 || player.velocity.x != 0)
         {
-            if (Mathf.Abs(player.velocity.x) >= player.stats.minSpeedToBrake)
-            {
-                player.state.ChangeState<BrakePlayerState>();
-            }
-            else
-            {
-                if (player.velocity.x > 0)
-                {
-                    player.velocity.x = -player.stats.turnSpeed;
-                }
-                else
-                {
-                    player.velocity.x = player.stats.turnSpeed;
-                }
-            }
+            player.state.ChangeState<WalkPlayerState>();
         }
     }
 
     public override void Enter(CharControlMotor player)
     {
-        player.velocity.x = 0;
     }
     public override void Exit(CharControlMotor player)
     {
--- Project Presto/Assets/Scripts/CharaterScripts/States/LedgeGrabStateFront.cs
+++ Project Presto/Assets/Scripts/CharaterScripts/States/LedgeGrabStateFront.cs
@@ -6,35 +6,30 @@ public class LedgeGrabStateFront : PlayerState
 {
     public override void Step(CharControlMotor player, float deltaTime)
     {
+        player.UpdateDirection(player.input.horizontal);
+        player.HandleSlopeFactor(deltaTime);
+        player.HandleAcceleration(deltaTime);
+        player.HandleFriction(deltaTime);
+        player.HandleGravity(deltaTime);
+        player.HandleFall();
         if (!player.HandleLedgeCheck().Item1)
         {
             player.state.ChangeState<WalkPlayerState>();
         }
+
         if (player.HandleLedgeCheck().Item2)
         {
             player.state.ChangeState<LedgeGrabStateBack>();
         }
+
         if (player.input.actionDown)
         {
             player.HandleJump();
         }
-        if (player.input.horizontal != 0)
+
+        if (player.input.horizontal != 0 || player.velocity.x != 0)
         {
-            if (Mathf.Abs(player.velocity.x) >= player.stats.minSpeedToBrake)
-            {
-                player.state.ChangeState<BrakePlayerState>();
-            }
-            else
-            {
-                if (player.velocity.x > 0)
-                {
-                    player.velocity.x = -player.stats.turnSpeed;
-                }
-                else
-                {
-                    player.velocity.x = player.stats.turnSpeed;
-                }
-            }
+            player.state.ChangeState<WalkPlayerState>();
         }
     }
 
