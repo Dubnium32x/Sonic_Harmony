de1bea95967e505603ba978586371a7c5bddec90
noah the goodra <peterpan0413@live.com>
2020-07-16 01:00:11 -0500

    fix life double removal

--- Project Presto/Assets/Scripts/CharaterScripts/CharControlMotor.cs
+++ Project Presto/Assets/Scripts/CharaterScripts/CharControlMotor.cs
@@ -239,6 +239,7 @@ public class CharControlMotor : PlayerMotor
 
     public void ApplyDeath()
     {
+        if(Death == true) return;
         var scoreManager = ScoreManager.Instance;
 
         if (scoreManager)
@@ -248,6 +249,7 @@ public class CharControlMotor : PlayerMotor
 
         SetShield(PlayerShields.None);
         state.ChangeState<DiePlayerState>();
+        Death = true;
     }
     public void SetShield(PlayerShields shield)
     {
@@ -428,6 +430,7 @@ public class CharControlMotor : PlayerMotor
     }
     public void Respawn(Vector3 position, Quaternion rotation)
     {
+        Death = false;
         direction = 1;
         EnableCollision(true);
         velocity = Vector3.zero;
